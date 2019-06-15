e121a00c942e1524a22a5d1a9316c9b91a53387f
Oleg Sachev <cbi444@gmail.com>
2019-06-15 18:59:39 +0300

    Add validation for Question :title uniqueness.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, presence: true,
-                    length: { in: 15..75 }
+                    length: { in: 15..75 },
+                    uniqueness: { case_sensitive: false }
   validates :body,  presence: true,
                     length: { in: 50..500 }
 
