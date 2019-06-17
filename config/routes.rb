bdb2c3807317763a2c9a5c415a675626c6708537
Oleg Sachev <cbi444@gmail.com>
2019-06-17 10:47:13 +0300

    Update routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :questions do
-    resources :answers, shallow: true, except: :index
+    resources :answers, shallow: true, except: %i[index show]
   end
 end
