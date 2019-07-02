842607d04db42723caf795779e7e6b45ac165233
Oleg Sachev <cbi444@gmail.com>
2019-07-02 19:15:23 +0300

    Implement scenario 'user adds invalid link when asks a question'.

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -9,10 +9,12 @@ feature 'user can add links to a question', %q{
   given(:gist_url1) { 'https://gist.github.com/metacorn/361365b73824b830cded8cd527850bc5' }
   given(:gist_url2) { 'https://gist.github.com/metacorn/06f8fcade496200f530fdc5e54816b4e' }
 
-  scenario 'user adds several links when asks a question', js: true do
+  background 'authenticated user' do
     login user
     visit new_question_path
+  end
 
+  scenario 'user adds several links when asks a question', js: true do
     fill_in 'Title', with: 'Test question title'
     fill_in 'Body', with: "#{"body" * 25}"
 
@@ -34,5 +36,21 @@ feature 'user can add links to a question', %q{
 
     expect(page).to have_link 'My gist #1', href: gist_url1
     expect(page).to have_link 'My gist #2', href: gist_url2
+  end
+
+  scenario 'user adds invalid link when asks a question', js: true do
+    fill_in 'Title', with: 'Test question title'
+    fill_in 'Body', with: "#{"body" * 25}"
+
+    click_on 'Add a link'
+
+    within '#new-links .nested-fields:nth-child(1)' do
+      fill_in 'Name', with: 'My gist #1'
+      fill_in 'URL', with: 'gist_url1'
+    end
+
+    click_on 'Ask'
+
+    expect(page).to have_content 'is an invalid URL'
   end
 end
