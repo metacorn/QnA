2b0011128b42dc18caa2670f316ef1ee9cbdc514
Oleg Sachev <cbi444@gmail.com>
2019-07-04 02:29:55 +0300

    Implement feature 'user can delete links of his answer'.

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe LinksController, type: :controller do
   let(:user2) { create(:user) }
   let(:question1) { create(:question, user: user1) }
   let(:question2) { create(:question, user: user2) }
+  let(:answer1) { create(:answer, question: question2, user: user1) }
+  let(:answer2) { create(:answer, question: question1, user: user2) }
   let!(:question_link1) { create(:link, :valid, linkable: question1) }
   let!(:question_link2) { create(:link, :valid_gist, linkable: question2) }
+  let!(:answer_link1) { create(:link, :valid, linkable: answer1) }
+  let!(:answer_link2) { create(:link, :valid, linkable: answer2) }
 
   describe "DELETE #destroy" do
     context "authenticated user" do
@@ -19,14 +23,33 @@ RSpec.describe LinksController, type: :controller do
         expect { question_link1.reload }.to raise_error ActiveRecord::RecordNotFound
       end
 
+      it "deletes user's answer link" do
+        expect {
+          delete :destroy, params: { id: answer_link1 }, format: :js
+        }.to change(answer1.links, :count).by(-1)
+        expect { answer_link1.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+
       it "tries to delete another user's question link" do
         expect {
           delete :destroy, params: { id: question_link2 }, format: :js
         }.to_not change(question2.links, :count)
       end
 
+      it "tries to delete another user's answer link" do
+        expect {
+          delete :destroy, params: { id: answer_link2 }, format: :js
+        }.to_not change(answer2.links, :count)
+      end
+
       it "render destroy template after deleting of question link" do
         delete :destroy, params: { id: question_link1 }, format: :js
+
+        expect(response).to render_template :destroy
+      end
+
+      it "render destroy template after deleting of answer link" do
+        delete :destroy, params: { id: answer_link1 }, format: :js
 
         expect(response).to render_template :destroy
       end
@@ -36,6 +59,12 @@ RSpec.describe LinksController, type: :controller do
       expect {
         delete :destroy, params: { id: question_link1 }, format: :js
       }.to_not change(question1.links, :count)
+    end
+
+    it "unauthenticated user tries to delete answer link" do
+      expect {
+        delete :destroy, params: { id: answer_link1 }, format: :js
+      }.to_not change(answer1.links, :count)
     end
   end
 end
--- spec/features/answer/delete_links_spec.rb
+++ spec/features/answer/delete_links_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-feature "user can delete links of his question", %q{
+feature "user can delete links of his answer", %q{
   as an authenticated user
-  i'd like to be able to delete only my own questions links
+  i'd like to be able to delete only my own answers links
 } do
   given(:user1) { create(:user) }
   given(:user2) { create(:user) }
   given(:question1) { create(:question, user: user1) }
   given(:question2) { create(:question, user: user2) }
-  given!(:link1) { create(:link, :valid, linkable: question1) }
-  given!(:link2) { create(:link, :valid_gist, linkable: question2) }
+  given(:answer1) { create(:answer, question: question2, user: user1) }
+  given(:answer2) { create(:answer, question: question1, user: user2) }
+  given!(:link1) { create(:link, :valid, linkable: answer1) }
+  given!(:link2) { create(:link, :valid_gist, linkable: answer2) }
 
-  scenario "unauthenticated user tries to delete question links" do
+  scenario "unauthenticated user tries to delete answer links" do
     visit question_path(question1)
 
     expect(page).to_not have_link 'Delete link'
@@ -22,8 +24,8 @@ feature "user can delete links of his question", %q{
       login(user1)
     end
 
-    scenario "deletes links of his question", js: true do
-      visit question_path(question1)
+    scenario "deletes links of his answer", js: true do
+      visit question_path(question2)
 
       within "#link_#{link1.id}" do
         click_on 'Delete link'
@@ -33,9 +35,9 @@ feature "user can delete links of his question", %q{
     end
 
     scenario "tries to delete links of another user's question" do
-      visit question_path(question2)
+      visit question_path(question1)
 
-      within "#question_#{question2.id}" do
+      within "#question_#{question1.id}" do
         expect(page).to_not have_link "Delete link"
       end
     end
