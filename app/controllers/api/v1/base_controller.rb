32dad6409ba9a78bb78271b86460c2f4305f590b
Oleg Sachev <cbi444@gmail.com>
2019-08-08 21:50:00 +0300

    Add Api::V1::ProfilesController#index. Fix CanCanCan authorization for ProfilesController.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::BaseController < ApplicationController
 
   private
 
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 end
--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
-  skip_authorization_check
+  authorize_resource class: User
 
   def me
-    render json: current_resource_owner, serializer: UserSerializer
+    render json: current_user, serializer: UserSerializer
+  end
+
+  def index
+    render json: User.where.not(id: current_user.id), each_serializer: UserSerializer
   end
 end
